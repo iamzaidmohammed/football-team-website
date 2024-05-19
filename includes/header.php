5ec3e448abbd523b43049d18338a99022fb3f327
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-05-19 18:12:28 +0000

    Seperated header and footer into includes folder to stop repetition

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <nav>
+        <div class="logo-container">
+            <img src="./assets/images/team01.png" alt="Team Logo" class="logo" />
+        </div>
+        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-xmark"></i>
+        <div class="menu">
+            <ul class="links">
+                <li class="link"><a href="index.php">Home</a></li>
+                <li class="link"><a href="./pages/about.php">About</a></li>
+                <li class="link"><a href="./pages/team.php">Team</a></li>
+                <li class="link"><a href="./pages/matches.php">Matches</a></li>
+                <li class="link"><a href="./pages/contact.php">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <main>
