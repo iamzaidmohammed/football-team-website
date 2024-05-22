7a9ac8cc94fc193dced5c78f721e2382ce997060
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-05-22 17:13:56 +0000

    Team page completed

---
+++ pages/team.php
@@ -0,0 +1,191 @@
+<?php include '../includes/header.php' ?>
+<main id="team">
+    <h2 class="heading">Team</h2>
+
+    <div class="players-container">
+        <h2>Goalkeepers</h2>
+        <div class="players">
+            <div class="goalkeepers position">
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="players-container">
+        <h2>Defenders</h2>
+        <div class="players">
+            <div class="defenders position">
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="players-container">
+        <h2>Midfielders</h2>
+        <div class="players">
+            <div class="midfielders position">
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="players-container">
+        <h2>Strikers</h2>
+        <div class="players">
+            <div class="strikers position">
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-num">23</p>
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="players-container">
+        <h2>Manager</h2>
+        <div class="players">
+            <div class="manager position">
+                <div class="player">
+                    <img src="../assets/images/player.jpg" alt="Player image">
+                    <div class="player-info">
+                        <p class="player-name">Mohammed Kudus</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include '../includes/footer.php' ?>
