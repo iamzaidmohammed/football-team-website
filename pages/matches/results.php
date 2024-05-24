9d39aab26f0b571c031483404ae08689c06fa4f8
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-05-24 15:20:05 +0000

    Results page completed

--- pages/matches/results.php
+++ pages/matches/results.php
@@ -9,24 +9,49 @@
 
     <div class="container">
         <div class="match">
-            <div class="date">
+            <div class="match-date">
                 12th June, 2024
             </div>
             <div class="teams">
                 <div class="team">
-                    <p class="team-name">Manchester City</p>
-                    <img src="../assets/images/team01.png" alt="Home team logo" class="team-logo">
+                    <p class="team_name right">Manchester City</p>
+                    <img src="../../assets/images/team01.png" alt="Home team logo" class="team_logo">
                 </div>
-                <span class="vs">VS</span>
+                <div class="scores">
+                    <span class="home-score score">3</span>
+                    :
+                    <span class="away-score score">2</span>
+                </div>
                 <div class="team">
-                    <img src="../assets/images/team01.png" alt="Away team logo" class="team-logo">
-                    <p class="team-name">Manchester City</p>
+                    <img src="../../assets/images/team01.png" alt="Away team logo" class="team_logo">
+                    <p class="team_name">Manchester City</p>
                 </div>
             </div>
-            </ </div>
         </div>
+        <div class="match">
+            <div class="match-date">
+                12th June, 2024
+            </div>
+            <div class="teams">
+                <div class="team">
+                    <p class="team_name right">Manchester City</p>
+                    <img src="../../assets/images/team01.png" alt="Home team logo" class="team_logo">
+                </div>
+                <div class="scores">
+                    <span class="home-score score">3</span>
+                    :
+                    <span class="away-score score">2</span>
+                </div>
+                <div class="team">
+                    <img src="../../assets/images/team01.png" alt="Away team logo" class="team_logo">
+                    <p class="team_name">Manchester City</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </main>
 
 <?php include './includes/footer.php' ?>
+
 <script src="../../js/toggleTabs.js"></script>
